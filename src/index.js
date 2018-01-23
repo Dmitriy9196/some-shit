8e652c62f2424b0c3d41af2fb6999d8bcbd55d62
Dmitriy <30418436+Dmitriy9196@users.noreply.github.com>
2018-01-24 02:41:40 +0500

    Some msg

--- src/index.js
+++ src/index.js
@@ -6,21 +6,11 @@ const NoteList = {
         let id = NoteList.Notes.length + 1;
         let noteName = document.querySelector('#input-name').value;
         NoteList.Notes.push({name: noteName, id: id});
+        localStorage.setItem(String(id), noteName);
         },
 
-    renderer: () => {
-        for (let i = 0; i < NoteList.Notes.length ; i++) {
-            let ul = document.querySelector('#ul-noteList');
-            let newLi = document.createElement('li');
-            newLi.innerText = NoteList.Notes[i].name;
-            newLi.classList.add('list-group-item');
-            ul.appendChild(newLi);
-        }
-    }
+    delNote: () => {},
+
+    renderer: () => {},
+
 };
-
-
-let buttonAddNote = document.querySelector('#button-addNote');
-let buttonDelNote = document.querySelector('#button-delNote');
-buttonAddNote.addEventListener('click', NoteList.addNote());
-buttonDelNote.addEventListener('click', NoteList.renderer());
