8a17dff3a889b819942f847f383c0b3e3c0d1b2b
Dmitriy <30418436+Dmitriy9196@users.noreply.github.com>
2018-01-24 02:06:23 +0500

    Some msg

--- src/index.js
+++ src/index.js
@@ -1,45 +1,20 @@
-var buttonAddNote = document.getElementById("1");
-var buttonDeleteNote = document.getElementById("2"), noteName;
-buttonAddNote.addEventListener("click", addNote);
-buttonDeleteNote.addEventListener("click", deleteNote);
+const NoteList = {
 
-var Note = function (name, desc) {
-    this.noteName = name;
+    Notes: [],
 
-    this.noteDesc = desc;
+    addNote: () => {
+        let id = NoteList.Notes.length + 1;
+        let noteName = document.querySelector('#input-name').value;
+        NoteList.Notes.push({name: noteName, id: id});
+        },
 
-    this.noteTemplate = function () {
-        var ul = document.getElementsByTagName("ul");
-
-/*        var ul = document.getElementsByTagName("ul");
-        var newNode = document.createElement("li");
-        var newDesc = document.createElement("span");
-        var descToNote = newNode[0].appendChild(newDesc);
-        var createdNote = ul[0].appendChild(newNode);
-        createdNote.innerHTML = this.noteName;*/
+    renderer: () => {
+        for (let i = 0; i < NoteList.Notes.length ; i++) {
+            let ul = document.querySelector('#ul-noteList');
+            let newLi = document.createElement('li');
+            newLi.innerText = NoteList.Notes[i].name;
+            newLi.classList.add('list-group-item');
+            ul.appendChild(newLi);
+        }
     }
 };
-
-
-
-function addNote() {
-    var noteNameInputValue = document.getElementsByName("noteName"); //берем поле для ввода текста (коллекция)
-    var value = noteNameInputValue[0].value; // достаем его значение (строка)
-    var desc = "Some text";
-    var newNote = new Note(value, desc);
-    newNote.noteTemplate();
-}
-
-function deleteNote() {
-    var last = document.getElementsByTagName("ul")[0].lastChild;
-    if (last === null) {
-        alert('nothing to remove');
-    }
-    else {
-        last.remove();
-    }
-}
-
-/*function manageNote() {
-
-}*/
