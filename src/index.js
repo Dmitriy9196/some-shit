629d54203f59c89ab99d40f2132b50bf17d32110
Dmitriy9196 <dimasuchkov1991@mail.ru>
2018-01-28 02:26:50 +0500

    Testing

--- src/index.js
+++ src/index.js
@@ -6,15 +6,16 @@ const NoteList = {
         const i = NoteList.index;
         let ul = document.querySelector('#ul-noteList');
         let noteName = document.querySelector('#input-name').value;
-        let noteDesc = document.querySelector('#input-desc').value;
-        let newNote = document.createElement('li');
-        let newNoteDesc = document.createElement('span');
-        newNote.classList.add('list-group-item');
-        newNoteDesc.classList.add('list-group-item');
-        newNote.setAttribute('id', `${i}`);
+        //let noteDesc = document.querySelector('#input-desc').value;
+        let newNote = makeElement('li', 'list-group-item', 'id', `${i}`);
+        // let newNote = document.createElement('li');
+        // let newNoteDesc = document.createElement('span');
+        // newNote.classList.add('list-group-item');
+        // newNoteDesc.classList.add('list-group-item');
+        // newNote.setAttribute('id', `${i}`);
         newNote.innerText = noteName;
-        newNoteDesc.innerText = noteDesc;
-        newNote.appendChild(newNoteDesc);
+        // newNoteDesc.innerText = noteDesc;
+        // newNote.appendChild(newNoteDesc);
         ul.appendChild(newNote);
         NoteList.index += 1;
     },
